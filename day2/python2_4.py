dd3767f9826323d32d6e57b0d11c26a147d7ac34
unknown <https://github.com/ohs6681/python.git>
2016-10-22 14:27:03 +0900

    day2 실습 소스

---
+++ day2/python2_4.py
@@ -0,0 +1,11 @@
+# 함수를 만들어 봅니다.
+def customPrint(str):
+    print("프린트:" + str)
+
+customPrint("안녕")
+customPrint("못")
+customPrint("참")
+customPrint("겠")
+customPrint("다")
+customPrint("빼")
+customPrint("액")
